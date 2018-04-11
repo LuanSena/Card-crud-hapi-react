5fd79af076e8b5fcf5bd9d800522f2a442b2778e
luan sena <luansena@live.com>
2018-04-11 20:54:53 -0300

    implements card props

--- dashboard/src/components/cardRepresentation/index.js
+++ dashboard/src/components/cardRepresentation/index.js
@@ -9,15 +9,15 @@ class CardRepresentation extends Component {
         <div className="card__front">
           <div className="card__logo top-right"></div>
           <div className="card__chip"></div>
-          <div className="card__number"></div>
-          <div className="card__holder-name"></div>
-          <div className="card__exp"></div>
+          <div className="card__number">{this.props.number}</div>
+          <div className="card__holder-name">{this.props.name}</div>
+          <div className="card__exp">{this.props.expiration}</div>
         </div>
       </div>
       <div className="flip__back">
         <div className="card__back">
           <div className="card__strip"></div>
-          <div className="card__ccv"></div>
+          <div className="card__ccv">{this.props.cvv}</div>
           <div className="card__logo bottom-right"></div>
         </div>
       </div>
