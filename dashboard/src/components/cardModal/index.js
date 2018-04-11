b8e54213a28e6a7bd9139f9b8f618cd6f1df2e3e
luan sena <luansena@live.com>
2018-04-11 20:55:12 -0300

    implements handlers to form fields

--- dashboard/src/components/cardModal/index.js
+++ dashboard/src/components/cardModal/index.js
@@ -3,13 +3,48 @@ import { Modal, Button, FormGroup, ControlLabel, FormControl } from 'react-boots
 import CardRepresentation from '../cardRepresentation'
 
 class CardModal extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { 
+      name: '',
+      expiration: '',
+      cvv: '',
+      number: ''
+    }
+    this.handleChangeCvv = this.handleChangeCvv.bind(this)
+    this.handleChangeExpiration = this.handleChangeExpiration.bind(this)
+    this.handleChangeName = this.handleChangeName.bind(this)
+    this.handleChangeNumber = this.handleChangeNumber.bind(this)
+  }
+
+  async handleChangeCvv (event) {
+    await this.setState({ cvv: event.target.value })
+  }
+
+  async handleChangeExpiration (event) {
+    await this.setState({ expiration: event.target.value })
+  }
+
+  async handleChangeName (event) {
+    await this.setState({ name: event.target.value })
+  }
+
+  async handleChangeNumber (event) {
+    await this.setState({ number: event.target.value })
+  }
+
   render() {
     return (<Modal show={this.props.show} onHide={this.handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Cadastro do cartão</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <CardRepresentation/>
+        <CardRepresentation 
+          name={this.state.name}
+          number={this.state.number} 
+          cvv={this.state.cvv}
+          expiration={this.state.expiration}
+        />
         <form className="form-inline">
         <FormGroup
           controlId="formBasicText"
@@ -18,30 +53,30 @@ class CardModal extends Component {
           <FormControl
             style={{width: '40%', borderRadius: 0}}
             type="text"
-            // value={this.state.value}
+            value={this.state.number}
             placeholder="Número do cartão"
-            // onChange={this.handleChange}
+            onChange={this.handleChangeNumber}
           />
           <FormControl
             style={{width: '40%', borderRadius: 0}}
             type="text"
-            // value={this.state.value}
+            value={this.state.name}
             placeholder="Nome do portador"
-            // onChange={this.handleChange}
+            onChange={this.handleChangeName}
           />
           <FormControl
             style={{width: '20%', borderRadius: 0}}
             type="text"
-            // value={this.state.value}
+            value={this.state.expiration}
             placeholder="Data de vencimento"
-            // onChange={this.handleChange}
+            onChange={this.handleChangeExpiration}
           />
           <FormControl
             style={{width: '20%', borderRadius: 0}}
             type="text"
-            // value={this.state.value}
+            value={this.state.cvv}
             placeholder="CVV"
-            // onChange={this.handleChange}
+            onChange={this.handleChangeCvv}
           />
           <Button
             style={{width: '30%', borderRadius: 0}}
@@ -52,9 +87,6 @@ class CardModal extends Component {
         </FormGroup>
       </form>
         </Modal.Body>
-        {/* <Modal.Footer>
-          <Button onClick={this.handleClose}>Close</Button>
-        </Modal.Footer> */}
       </Modal>)
   
   }
