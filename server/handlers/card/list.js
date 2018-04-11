6ba8785b6eb55e5276816a861d57d121af683a6c
Luan Sena <luansena@live.com>
2018-04-11 00:59:04 -0300

    implements card listing with decrypt

--- server/handlers/card/list.js
+++ server/handlers/card/list.js
@@ -2,11 +2,15 @@
 
 module.exports = async function (request, h) {
   const Boom = require('hapi');
-  const pool = request.mysql.pool    
+  const jwtDecoder = require('jwt-js').decodeToken
+  const pool = request.mysql.pool
   try {
     const [rows, fields] = await pool.query('select * from card limit 10;')
-    return h.response(rows).code(200)
+    const openedRows = rows.map(row => Object.assign({}, row))
+    .map(obj => Object.assign({}, obj, jwtDecoder(obj.HASH).payload))
+    return h.response(openedRows).code(200)
   } catch (err) {
+    console.log(err)
     throw Boom.internal('Internal Mysql Error', err)
   }
 }
