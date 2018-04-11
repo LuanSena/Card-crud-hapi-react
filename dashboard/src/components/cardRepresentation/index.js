f79432b0b7310c20528d39f6f491d7a6f5bbd8ba
Luan Sena <luansena@live.com>
2018-04-11 11:06:24 -0300

    adds card representation component with half implemetation

---
+++ dashboard/src/components/cardRepresentation/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class CardRepresentation extends Component {
+  render() {
+    return (<div className="card">
+    <div className="flip">
+      <div className="flip__front shown">
+        <div className="card__front">
+          <div className="card__logo top-right"></div>
+          <div className="card__chip"></div>
+          <div className="card__number"></div>
+          <div className="card__holder-name"></div>
+          <div className="card__exp"></div>
+        </div>
+      </div>
+      <div className="flip__back">
+        <div className="card__back">
+          <div className="card__strip"></div>
+          <div className="card__ccv"></div>
+          <div className="card__logo bottom-right"></div>
+        </div>
+      </div>
+    </div>
+  </div>)
+  
+  }
+}
+
+export default CardRepresentation;
