615e2cbe754f570e0ddb969c8d448171ec4a29bd
Luan Sena <luansena@live.com>
2018-05-01 17:48:13 -0300

    warps redux store

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
 import Card from './components/card';
 import registerServiceWorker from './registerServiceWorker';
+import Store from './components/support/store';
 
-ReactDOM.render(<Card />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={Store}> 
+      <Card />
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
